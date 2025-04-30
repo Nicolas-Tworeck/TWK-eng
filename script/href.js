e9c84abef02417618c31e0b703644e5e42a62477
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-04-30 13:27:29 -0300

    commit

---
+++ script/href.js
@@ -0,0 +1,31 @@
+function sobre(){
+    window.location.href = "#sobre";
+}
+function service(){
+    window.location.href = "#service";
+}
+function projeto(){
+    window.location.href = "#projeto";
+}
+function cliente(){
+    window.location.href = "#cliente";
+}
+function contato(){
+    window.location.href = "#contato";
+}
+
+function whatsTWK(){
+    window.location.href = "https://api.whatsapp.com/send?phone=554298690111&text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20para%20minha%20ind%C3%BAstria";
+}
+
+function mailTWK(){
+    window.location.href = "mailto:engenharia@twkeng.com.br";
+}
+
+function instaEli(){
+    window.location.href = "https://www.instagram.com/tworeckeliezer?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
+}
+
+function siteDev(){
+    window.location.href = "https://crealitytech.com.br/";
+}
