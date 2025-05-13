ce82f7277b26a2f3868ec8fadd7667dace996a95
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-05-13 16:44:03 -0300

    commit

--- script/href.js
+++ script/href.js
@@ -14,6 +14,16 @@ function contato(){
     window.location.href = "#contato";
 }
 
+function clientePage(){
+    window.location.href = "clientes.html";
+}
+function home(){
+    window.location.href = "index.html";
+}
+
+function areaClientePage(){
+    window.location.href = "#clienteArea";
+}
 function whatsTWK(){
     window.location.href = "https://api.whatsapp.com/send?phone=554298690111&text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20para%20minha%20ind%C3%BAstria";
 }
